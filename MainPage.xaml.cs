f750d08e0c5449859198aca80beb40b03a5f1b06
SophyaneT <50209039+SophyaneT@users.noreply.github.com>
2019-05-17 10:17:09 +0200

    Application mobile
    Voici les codes de l'application mobile

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Android.Bluetooth;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectMobileDOM
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ChangePageAsync(object sender, EventArgs e)
+        {
+            if (Username.Text == "admin" && Pwd.Text == "admin")
+                await Navigation.PushModalAsync(new Home());
+            else
+                errorPwd.IsVisible = true;
+
+        }
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
